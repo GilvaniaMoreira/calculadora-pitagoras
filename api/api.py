b1546c29ed3f100acb24523c1e11dec1f8b41c8f
GilvaniaMoreira <moreiragil84@gmail.com>
2022-09-30 00:20:18 -0300

    Adicionado validação se a hipotenusa é menor que os catetos

--- api/api.py
+++ api/api.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 from flask import Flask, request, jsonify
 import math
  
@@ -5,31 +6,38 @@ app = Flask(__name__)
 
 @app.route('/calcular', methods = ['POST'])
 def calcular():
-    a = request.form.get('a', None)
-    b = request.form.get('b', None)
-    c = request.form.get('c', None)
-    precisao = request.form.get('precisao', 2)
+    try: 
+        a = request.form.get('a', None)
+        b = request.form.get('b', None)
+        c = request.form.get('c', None)
+        precisao = request.form.get('precisao', 2)
 
-    #TODO: Verificar se os valores da hipotenusa é maior que os catetos
-
-    if not a and b and c:
-        a = math.sqrt(int(b) ** 2 + int(c) ** 2)
-        response = jsonify({"a":str(round(a,int(precisao)))})
-        response.status_code = 200
-    elif not b and a and c:
-        b = math.sqrt(int(a) ** 2 - int(c) ** 2)
-        response = jsonify({"b":str(round(b,int(precisao)))})
-        response.status_code = 200
-    elif not c and a and b:
-        c = math.sqrt(int(a) ** 2 - int(b) ** 2)
-        response = jsonify({"c":str(round(c,int(precisao)))})
-        response.status_code = 200
-    else:
-        response = jsonify("")
-        response.status_code = 400
+        if not a and b and c:
+            a = math.sqrt(float(b) ** 2 + float(c) ** 2)
+            response = jsonify({"a":str(round(a,int(precisao)))})            
+        elif not b and a and c:
+            if float(a) < float(c):
+                response = jsonify({"error": "A hipotenusa não pode ser maior que os catetos"})
+            else:
+                b = math.sqrt(float(a) ** 2 - float(c) ** 2)
+                response = jsonify({"b":str(round(b,int(precisao)))})                
+        elif not c and a and b:
+            if float(a) < float(b):
+                response = jsonify({"error": "A hipotenusa não pode ser maior que os catetos"})
+            else:
+                c = math.sqrt(float(a) ** 2 - float(b) ** 2)
+                response = jsonify({"c":str(round(c,int(precisao)))})                
+        else:
+            response = jsonify("")            
+    
+    except ValueError:
+        response = jsonify({"error": "Valor digitado deve ser um número"})
+    except Exception as e:
+        response = jsonify({"error": "Ocorreu um error: %s" %e})
 
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
 
 if __name__ == "__main__":
     app.run(debug=True)
